bf9142037df7527120e8709e6ac9d6ef5895516e
Faith  Gicheha <fwgicheh@gmail.com>
2020-07-02 02:00:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,10 @@
 class Artist
   @@all = []
-  attr_accessor :name
-  def initialize(name)
+  attr_accessor :name :songs
+  def initialize(name, songs)
     @name = name
     @@all << self
+    @songs = song
   end
   def self.all
     @@all
