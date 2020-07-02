a1e3553025c71a65c167fa318d56b076248a4e55
Faith  Gicheha <fwgicheh@gmail.com>
2020-07-02 04:39:54 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,11 @@
+class Apointment 
+  @@all = [ ]
+  attr_accessor :date, :patient, :doctor
+  def initialize(date, patient, doctor)
+    @name = name
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+end 
