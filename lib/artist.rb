573179bb87609b4542bad6f27ebd089e1e750678
Faith  Gicheha <fwgicheh@gmail.com>
2020-07-02 03:26:34 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,14 +12,14 @@ class Artist
     @@all
   end
 
-#   def songs
-#    Song.all.select {|song| song.artist == self}
-# end
-# def add_song(song)
-#    song.artist = self
-#  end
-#
-#  def songs
-#    Song.all.select {|song| song.artist == self}
-#  end
+  def songs
+   Song.all.select {|song| song.artist == self}
+end
+def add_song(song)
+   song.artist = self
+ end
+
+ # def songs
+ #   Song.all.select {|song| song.artist == self}
+ # end
 end
