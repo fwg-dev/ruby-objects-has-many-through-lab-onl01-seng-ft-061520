2f79b0827c41415f12074d10b4c4bd10703a9381
Faith  Gicheha <fwgicheh@gmail.com>
2020-07-02 03:05:56 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,19 +1,20 @@
 class Artist
   @@all = []
-  attr_accessor :name, :genre
+  attr_accessor :name, :genre, :songs
   def initialize(name)
     @name = name
     @@all << self
     @songs = []
     @genre = "rap"
+    @songs = []
   end
   def self.all
     @@all
   end
 
- #  def songs(genre)
- #    @songs = "rap"
- #  end
+  def songs(genre)
+    @songs = "rap"
+  end
 
  # def songs
  #    Song.all.select {|song| song.artist == self}
