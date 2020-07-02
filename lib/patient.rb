17a81673e0aff3242c8ed54c9f4d97e81dc7667d
Faith  Gicheha <fwgicheh@gmail.com>
2020-07-02 05:16:36 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -8,8 +8,12 @@ class Patient
   def self.all
     @@all
   end
-
+  def new_appointment(date, self,patient)
+    Appointment.new(date, patient)
+  end
   def appointments
       Appointment.all {|appointment| appointment.patient == self}
 end
+
+def 
 end
